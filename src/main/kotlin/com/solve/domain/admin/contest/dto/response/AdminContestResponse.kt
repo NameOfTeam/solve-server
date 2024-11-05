fa2655e3107cdec2b18b6e81b5d307fd0563ef51
전민오 <jbj338033@gmail.com>
2024-11-05 21:45:12 +0900

    feat: admin contest

--- src/main/kotlin/com/solve/domain/admin/contest/dto/response/AdminContestResponse.kt
+++ src/main/kotlin/com/solve/domain/admin/contest/dto/response/AdminContestResponse.kt
@@ -1,8 +1,13 @@
 package com.solve.domain.admin.contest.dto.response
 
 import com.solve.domain.contest.domain.entity.Contest
+import com.solve.domain.contest.domain.entity.ContestOperator
+import com.solve.domain.contest.domain.entity.ContestParticipant
+import com.solve.domain.contest.domain.entity.ContestProblem
 import com.solve.domain.contest.domain.enums.ContestVisibility
+import com.solve.domain.user.domain.entity.User
 import java.time.LocalDateTime
+import java.util.*
 
 data class AdminContestResponse(
     val id: Long,
@@ -11,7 +16,7 @@ data class AdminContestResponse(
     val startAt: LocalDateTime,
     val endAt: LocalDateTime,
     val owner: AdminContestOwnerResponse,
-    val participants: List<AdminContestParticipant>,
+    val participants: List<AdminContestParticipantResponse>,
     val operators: List<AdminContestOperatorResponse>,
     val problems: List<AdminContestProblemResponse>,
     val visibility: ContestVisibility,
@@ -27,11 +32,57 @@ data class AdminContestResponse(
             endAt = contest.endAt,
             createdAt = contest.createdAt,
             updatedAt = contest.updatedAt,
-            participants = contest.participants.map { AdminContestParticipant.of(it) },
+            participants = contest.participants.map { AdminContestParticipantResponse.of(it) },
             operators = contest.operators.map { AdminContestOperatorResponse.of(it) },
             problems = contest.problems.map { AdminContestProblemResponse.of(it) },
             visibility = contest.visibility,
             owner = AdminContestOwnerResponse.of(contest.owner)
+        )
+    }
+}
+
+data class AdminContestProblemResponse(
+    val title: String,
+) {
+    companion object {
+        fun of(problem: ContestProblem) = AdminContestProblemResponse(
+            title = problem.problem.title
+        )
+    }
+}
+
+data class AdminContestParticipantResponse(
+    val id: UUID,
+    val username: String
+) {
+    companion object {
+        fun of(participant: ContestParticipant) = AdminContestParticipantResponse(
+            id = participant.user.id!!,
+            username = participant.user.username
+        )
+    }
+}
+
+data class AdminContestOwnerResponse(
+    val id: UUID,
+    val username: String
+) {
+    companion object {
+        fun of(owner: User) = AdminContestOwnerResponse(
+            id = owner.id!!,
+            username = owner.username
+        )
+    }
+}
+
+data class AdminContestOperatorResponse(
+    val id: UUID,
+    val username: String,
+) {
+    companion object {
+        fun of(operator: ContestOperator) = AdminContestOperatorResponse(
+            id = operator.user.id!!,
+            username = operator.user.username
         )
     }
 }
