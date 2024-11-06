d92e25e08d922d2538acaf23346345d29ba2557c
전민오 <jbj338033@gmail.com>
2024-11-06 15:49:31 +0900

    fix: block multiple connection

--- src/main/kotlin/com/solve/domain/user/service/impl/UserMeConnectionServiceImpl.kt
+++ src/main/kotlin/com/solve/domain/user/service/impl/UserMeConnectionServiceImpl.kt
@@ -3,8 +3,10 @@ package com.solve.domain.user.service.impl
 import com.solve.domain.user.domain.entity.UserConnection
 import com.solve.domain.user.dto.request.UserMeAddConnectionRequest
 import com.solve.domain.user.dto.response.UserMeResponse
+import com.solve.domain.user.error.UserConnectionError
 import com.solve.domain.user.repository.UserRepository
 import com.solve.domain.user.service.UserMeConnectionService
+import com.solve.global.error.CustomException
 import com.solve.global.security.holder.SecurityHolder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -18,6 +20,11 @@ class UserMeConnectionServiceImpl(
     @Transactional
     override fun addConnection(request: UserMeAddConnectionRequest): UserMeResponse {
         val user = securityHolder.user
+
+        if (user.connections.any { it.type == request.type }) throw CustomException(
+            UserConnectionError.CONNECTION_ALREADY_EXISTS,
+            request.type.name
+        )
 
         user.connections.add(
             UserConnection(
@@ -35,8 +42,11 @@ class UserMeConnectionServiceImpl(
     @Transactional
     override fun removeConnection(connectionId: UUID): UserMeResponse {
         val user = securityHolder.user
+        val connection = user.connections.find { it.id == connectionId } ?: throw CustomException(
+            UserConnectionError.CONNECTION_NOT_FOUND
+        )
 
-        user.connections.removeIf { it.id == connectionId }
+        user.connections.remove(connection)
 
         userRepository.save(user)
 
