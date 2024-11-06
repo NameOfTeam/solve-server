3bcf2fd253430f4f188188bf071ef5ad0a46cbf0
전민오 <jbj338033@gmail.com>
2024-11-06 19:34:50 +0900

    fix: admin connection add/remove

--- src/main/kotlin/com/solve/domain/admin/user/controller/AdminUserConnectionController.kt
+++ src/main/kotlin/com/solve/domain/admin/user/controller/AdminUserConnectionController.kt
@@ -1,6 +1,8 @@
 package com.solve.domain.admin.user.controller
 
 import com.solve.domain.admin.user.dto.request.AdminUserConnectionAddRequest
+import com.solve.domain.admin.user.service.AdminUserConnectionService
+import com.solve.global.common.BaseResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
@@ -9,14 +11,14 @@ import java.util.*
 @Tag(name = "관리자: 사용자 연결", description = "Admin: User Connection")
 @RestController
 @RequestMapping("/admin/users/{userId}/connections")
-class AdminUserConnectionController {
+class AdminUserConnectionController(
+    private val adminUserConnectionService: AdminUserConnectionService
+) {
     @Operation(summary = "사용자 연결 추가")
     @PostMapping
-    fun addUserConnection(@PathVariable userId: UUID, @RequestBody request: AdminUserConnectionAddRequest) {
-    }
+    fun addUserConnection(@PathVariable userId: UUID, @RequestBody request: AdminUserConnectionAddRequest) = BaseResponse.of(adminUserConnectionService.addUserConnection(userId, request), 201)
 
     @Operation(summary = "사용자 연결 삭제")
     @DeleteMapping("/{connectionId}")
-    fun deleteUserConnection(@PathVariable userId: UUID, @PathVariable connectionId: UUID) {
-    }
+    fun removeUserConnection(@PathVariable userId: UUID, @PathVariable connectionId: UUID) = BaseResponse.of(adminUserConnectionService.removeUserConnection(userId, connectionId), 204)
 }
