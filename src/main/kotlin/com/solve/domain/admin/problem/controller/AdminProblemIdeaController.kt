17f028c0ae169a052c143f65e96fd16013c7a5ff
전민오 <jbj338033@gmail.com>
2025-01-09 08:06:18 +0900

    feat: add get mapping

--- src/main/kotlin/com/solve/domain/admin/problem/controller/AdminProblemIdeaController.kt
+++ src/main/kotlin/com/solve/domain/admin/problem/controller/AdminProblemIdeaController.kt
@@ -3,6 +3,7 @@ package com.solve.domain.admin.problem.controller
 import com.solve.global.common.dto.BaseResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -12,5 +13,6 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/admin/problems/{problemId}/ideas")
 class AdminProblemIdeaController {
     @Operation(summary = "문제 아이디어 목록 조회", description = "문제의 아이디어 목록을 조회합니다.")
+    @GetMapping
     fun getProblemIdeas(@PathVariable problemId: Long) = BaseResponse.of("getProblemIdeas")
 }
