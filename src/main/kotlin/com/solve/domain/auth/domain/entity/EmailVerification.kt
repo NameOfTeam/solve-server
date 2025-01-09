c2c4d9e3c0dd601959af5bde3b117c1161e1233f
전민오 <jbj338033@gmail.com>
2025-01-10 08:56:34 +0900

    feat: add time fields to email verification

--- src/main/kotlin/com/solve/domain/auth/domain/entity/EmailVerification.kt
+++ src/main/kotlin/com/solve/domain/auth/domain/entity/EmailVerification.kt
@@ -7,8 +7,7 @@ import java.time.LocalDateTime
 @Entity
 @Table(name = "email_verifications")
 class EmailVerification(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 
     @Column(name = "verification_token", nullable = false, unique = true, updatable = false)
