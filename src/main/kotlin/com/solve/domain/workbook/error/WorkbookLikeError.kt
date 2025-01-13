11d63708c6b5c805c1e479554a69d6bc2ae520a4
전민오 <jbj338033@gmail.com>
2025-01-13 20:33:58 +0900

    feat: reformat workbook like

--- src/main/kotlin/com/solve/domain/workbook/error/WorkbookLikeError.kt
+++ src/main/kotlin/com/solve/domain/workbook/error/WorkbookLikeError.kt
@@ -3,7 +3,7 @@ package com.solve.domain.workbook.error
 import com.solve.global.error.CustomError
 import org.springframework.http.HttpStatus
 
-enum class WorkbookLikeError(override val status: HttpStatus, override val message: String): CustomError {
+enum class WorkbookLikeError(override val status: HttpStatus, override val message: String) : CustomError {
     WORKBOOK_LIKE_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 좋아요를 누른 문제집입니다."),
     WORKBOOK_LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 누른 문제집을 찾을 수 없습니다.")
 }
