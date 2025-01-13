eb4731e9176e1e2169eb5bc5c535ede3835b61b0
전민오 <jbj338033@gmail.com>
2025-01-13 19:25:29 +0900

    refactor: remove duplicating code

--- src/main/kotlin/com/solve/domain/problem/util/config/LanguageConfig.kt
+++ src/main/kotlin/com/solve/domain/problem/util/config/LanguageConfig.kt
@@ -6,34 +6,28 @@ import java.io.File
 data class LanguageConfig (
     val name: String,
     val fileName: String,
-    val getSourceDirectory: (submitId: Long, basePath: String) -> File,
-    val getExecutionTarget: (submitId: Long) -> String
 ) {
+    fun getExecutionTarget(submitId: Long): String {
+        return "$submitId/${fileName}"
+    }
+
     companion object {
         val LANGUAGE_CONFIGS = mapOf(
             ProgrammingLanguage.PYTHON to LanguageConfig(
                 name = "python",
                 fileName = "main.py",
-                getSourceDirectory = { submitId, basePath -> File(basePath, "submits/$submitId") },
-                getExecutionTarget = { submitId -> "$submitId/main.py" }
             ),
             ProgrammingLanguage.JAVA to LanguageConfig(
                 name = "java",
                 fileName = "Main.java",
-                getSourceDirectory = { submitId, basePath -> File(basePath, "submits/$submitId") },
-                getExecutionTarget = { submitId -> "$submitId/Main.java" }
             ),
             ProgrammingLanguage.C to LanguageConfig(
                 name = "c",
                 fileName = "main.c",
-                getSourceDirectory = { submitId, basePath -> File(basePath, "submits/$submitId") },
-                getExecutionTarget = { submitId -> "$submitId/main.c" }
             ),
             ProgrammingLanguage.NODE_JS to LanguageConfig(
                 name = "node",
                 fileName = "main.js",
-                getSourceDirectory = { submitId, basePath -> File(basePath, "submits/$submitId") },
-                getExecutionTarget = { submitId -> "$submitId/main.js" }
             )
         )
     }
