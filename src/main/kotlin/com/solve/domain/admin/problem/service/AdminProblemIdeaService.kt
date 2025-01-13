61e60a513c141b0f81bf119df74f018d92daffa5
전민오 <jbj338033@gmail.com>
2025-01-13 20:25:29 +0900

    remove: problem idea

--- src/main/kotlin/com/solve/domain/admin/problem/service/AdminProblemIdeaService.kt
+++
@@ -1,7 +0,0 @@
-package com.solve.domain.admin.problem.service
-
-import com.solve.domain.admin.problem.dto.response.AdminProblemIdeaResponse
-
-interface AdminProblemIdeaService {
-    fun getProblemIdeas(problemId: Long): List<AdminProblemIdeaResponse>
-}
--- src/main/kotlin/com/solve/domain/problem/dto/request/ProblemIdeaCreateRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.solve.domain.problem.dto.request
-
-data class ProblemIdeaCreateRequest(
-    val title: String,
-    val content: String
-)
