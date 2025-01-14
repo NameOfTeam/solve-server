76978cb083d03046943f032084e08a6f726dec74
전민오 <jbj338033@gmail.com>
2025-01-14 14:46:01 +0900

    feat: add response to create post

--- src/main/kotlin/com/solve/domain/post/service/PostService.kt
+++ src/main/kotlin/com/solve/domain/post/service/PostService.kt
@@ -2,9 +2,10 @@ package com.solve.domain.post.service
 
 import com.solve.domain.post.dto.request.PostCreateRequest
 import com.solve.domain.post.dto.request.PostUpdateRequest
+import com.solve.domain.post.dto.response.PostCreateResponse
 
 interface PostService {
-    fun createPost(request: PostCreateRequest)
+    fun createPost(request: PostCreateRequest): PostCreateResponse
     fun updatePost(postId: Long, request: PostUpdateRequest)
     fun deletePost(postId: Long)
 }
