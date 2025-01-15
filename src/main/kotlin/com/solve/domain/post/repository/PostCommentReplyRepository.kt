dbac2b6e2d69af2cce8ff9d29a7457ca585935bb
전민오 <jbj338033@gmail.com>
2025-01-15 20:51:39 +0900

    feat: reply paging

--- src/main/kotlin/com/solve/domain/post/repository/PostCommentReplyRepository.kt
+++ src/main/kotlin/com/solve/domain/post/repository/PostCommentReplyRepository.kt
@@ -3,10 +3,13 @@ package com.solve.domain.post.repository
 import com.solve.domain.post.domain.entity.Post
 import com.solve.domain.post.domain.entity.PostComment
 import com.solve.domain.post.domain.entity.PostCommentReply
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PostCommentReplyRepository: JpaRepository<PostCommentReply, Long> {
     fun findAllByPostAndComment(post: Post, comment: PostComment): List<PostCommentReply>
+    fun findAllByPostAndCommentOrderByCreatedAt(post: Post, comment: PostComment, pageable: Pageable): Page<PostCommentReply>
     fun findByPostAndCommentAndId(post: Post, comment: PostComment, id: Long): PostCommentReply?
 
     fun countByComment(comment: PostComment): Long
--- src/main/kotlin/com/solve/domain/post/service/PostCommentReplyService.kt
+++ src/main/kotlin/com/solve/domain/post/service/PostCommentReplyService.kt
@@ -3,9 +3,11 @@ package com.solve.domain.post.service
 import com.solve.domain.post.dto.request.PostCommentReplyCreateRequest
 import com.solve.domain.post.dto.request.PostCommentReplyUpdateRequest
 import com.solve.domain.post.dto.response.PostCommentReplyResponse
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 
 interface PostCommentReplyService {
-    fun getReplies(postId: Long, commentId: Long): List<PostCommentReplyResponse>
+    fun getReplies(postId: Long, commentId: Long, pageable: Pageable): Page<PostCommentReplyResponse>
     fun createReply(postId: Long, commentId: Long, request: PostCommentReplyCreateRequest)
     fun updateReply(postId: Long, commentId: Long, replyId: Long, request: PostCommentReplyUpdateRequest)
     fun deleteReply(postId: Long, commentId: Long, replyId: Long)
