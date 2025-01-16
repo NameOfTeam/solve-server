304a1c79de648475b8ad672614f7850130d31851
전민오 <jbj338033@gmail.com>
2025-01-16 19:57:59 +0900

    feat: remove contest problem one to many

---
+++ src/main/kotlin/com/solve/domain/contest/repository/ContestProblemRepository.kt
@@ -0,0 +1,13 @@
+package com.solve.domain.contest.repository
+
+import com.solve.domain.contest.domain.entity.Contest
+import com.solve.domain.contest.domain.entity.ContestProblem
+import com.solve.domain.problem.domain.entity.Problem
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ContestProblemRepository : JpaRepository<ContestProblem, Long> {
+    fun findByContestAndProblem(contest: Contest, problem: Problem): ContestProblem?
+    fun findAllByContest(contest: Contest): List<ContestProblem>
+
+    fun existsByContestAndProblem(contest: Contest, problem: Problem): Boolean
+}
