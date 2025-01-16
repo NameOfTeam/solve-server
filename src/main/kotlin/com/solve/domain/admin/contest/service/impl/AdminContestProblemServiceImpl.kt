2a7f7b9521341abcfc8e6d3d2ab269253e551cf9
전민오 <jbj338033@gmail.com>
2025-01-16 19:58:30 +0900

    feat: remove one to many

--- src/main/kotlin/com/solve/domain/admin/contest/service/impl/AdminContestProblemServiceImpl.kt
+++ src/main/kotlin/com/solve/domain/admin/contest/service/impl/AdminContestProblemServiceImpl.kt
@@ -1,13 +1,11 @@
 package com.solve.domain.admin.contest.service.impl
 
 import com.solve.domain.admin.contest.dto.request.AdminContestProblemAddRequest
-import com.solve.domain.admin.contest.dto.response.*
 import com.solve.domain.admin.contest.service.AdminContestProblemService
-import com.solve.domain.contest.domain.entity.Contest
 import com.solve.domain.contest.domain.entity.ContestProblem
 import com.solve.domain.contest.error.ContestError
 import com.solve.domain.contest.error.ContestProblemError
-import com.solve.domain.contest.repository.ContestOperatorRepository
+import com.solve.domain.contest.repository.ContestProblemRepository
 import com.solve.domain.contest.repository.ContestRepository
 import com.solve.domain.problem.error.ProblemError
 import com.solve.domain.problem.repository.ProblemRepository
@@ -19,7 +17,8 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class AdminContestProblemServiceImpl(
     private val contestRepository: ContestRepository,
-    private val problemRepository: ProblemRepository
+    private val problemRepository: ProblemRepository,
+    private val contestProblemRepository: ContestProblemRepository
 ) : AdminContestProblemService {
     @Transactional
     override fun addContestProblem(contestId: Long, request: AdminContestProblemAddRequest) {
@@ -28,20 +27,21 @@ class AdminContestProblemServiceImpl(
         val problem =
             problemRepository.findByIdOrNull(request.problemId) ?: throw CustomException(ProblemError.PROBLEM_NOT_FOUND)
 
-        if (contest.problems.any { it.problem == problem }) {
+        if (contestProblemRepository.existsByContestAndProblem(contest, problem))
             throw CustomException(ContestProblemError.CONTEST_PROBLEM_ALREADY_EXISTS)
-        }
 
-        contest.problems.add(ContestProblem(contest = contest, problem = problem))
+        contestProblemRepository.save(ContestProblem(contest = contest, problem = problem))
     }
 
     @Transactional
     override fun removeContestProblem(contestId: Long, problemId: Long) {
         val contest =
             contestRepository.findByIdOrNull(contestId) ?: throw CustomException(ContestError.CONTEST_NOT_FOUND)
-        val problem = contest.problems.find { it.problem.id == problemId }
-            ?: throw CustomException(ProblemError.PROBLEM_NOT_FOUND)
+        val problem =
+            problemRepository.findByIdOrNull(problemId) ?: throw CustomException(ProblemError.PROBLEM_NOT_FOUND)
+        val contestProblem = contestProblemRepository.findByContestAndProblem(contest, problem)
+            ?: throw CustomException(ContestProblemError.CONTEST_PROBLEM_NOT_FOUND)
 
-        contest.problems.remove(problem)
+        contestProblemRepository.delete(contestProblem)
     }
 }
