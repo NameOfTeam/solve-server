5dd5ed03ed415db1dae5307faa1f00e6ba9bea3b
su080915 <tbr335@naver.com>
2025-01-16 11:52:51 +0900

    fix: run controller

--- src/main/kotlin/com/solve/domain/problem/controller/ProblemRunController.kt
+++ src/main/kotlin/com/solve/domain/problem/controller/ProblemRunController.kt
@@ -1,26 +1,23 @@
 package com.solve.domain.problem.controller
 
 import com.solve.domain.problem.dto.request.RunCodeRequest
+import com.solve.domain.problem.dto.response.ProblemRunResponse
 import com.solve.domain.problem.service.impl.ProblemRunService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "코드 실행", description = "코드를 실행합니다.")
 @RestController
 @RequestMapping("/problems/runs")
 class ProblemRunController(
     private val problemRunService: ProblemRunService
 ) {
+    @Operation(summary = "코드 실행", description = "코드를 실행합니다.")
     @PostMapping
-    fun runCode(@RequestBody request: RunCodeRequest) : ResponseEntity<ExecuteResponse>{
+    fun runCode(@RequestBody request: RunCodeRequest) : ResponseEntity<ProblemRunResponse>{
         val runId = problemRunService.initializeRun(request)
-        return ResponseEntity.ok(ExecuteResponse(
-            runId = runId,
-            wsUrl = "/ws/run/$runId"  // WebSocket 연결을 위한 URL 반환
-        ))
+        return ResponseEntity.ok(ProblemRunResponse(runId = runId))
     }
 }
-
-data class ExecuteResponse(
-    val runId: String,
-    val wsUrl: String
-)
---
+++ src/main/kotlin/com/solve/domain/problem/dto/response/ProblemRunReponse.kt
@@ -0,0 +1,5 @@
+package com.solve.domain.problem.dto.response
+
+data class ProblemRunResponse(
+    val runId: String,
+)
