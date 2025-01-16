263393e5ee490920add4c063a506da84e50a744f
전민오 <jbj338033@gmail.com>
2025-01-16 19:56:15 +0900

    feat: contest mapper

--- src/main/kotlin/com/solve/domain/contest/service/impl/ContestServiceImpl.kt
+++ src/main/kotlin/com/solve/domain/contest/service/impl/ContestServiceImpl.kt
@@ -1,10 +1,8 @@
 package com.solve.domain.contest.service.impl
 
-import com.solve.domain.contest.domain.entity.Contest
-import com.solve.domain.contest.domain.enums.ContestState
-import com.solve.domain.contest.dto.response.*
+import com.solve.domain.contest.dto.response.ContestResponse
 import com.solve.domain.contest.error.ContestError
-import com.solve.domain.contest.repository.ContestOperatorRepository
+import com.solve.domain.contest.mapper.ContestMapper
 import com.solve.domain.contest.repository.ContestRepository
 import com.solve.domain.contest.service.ContestService
 import com.solve.global.error.CustomException
@@ -13,16 +11,15 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
 
 @Service
 class ContestServiceImpl(
     private val contestRepository: ContestRepository,
-    private val contestOperatorRepository: ContestOperatorRepository
+    private val contestMapper: ContestMapper
 ) : ContestService {
     @Transactional(readOnly = true)
     override fun getContests(pageable: Pageable): Page<ContestResponse> {
-        return contestRepository.findAll(pageable).map { it.toResponse() }
+        return contestRepository.findAll(pageable).map { contestMapper.toResponse(it) }
     }
 
     @Transactional(readOnly = true)
@@ -30,29 +27,6 @@ class ContestServiceImpl(
         val contest =
             contestRepository.findByIdOrNull(contestId) ?: throw CustomException(ContestError.CONTEST_NOT_FOUND)
 
-        return contest.toResponse()
+        return contestMapper.toResponse(contest)
     }
-
-    private fun Contest.toResponse() = ContestResponse(
-        id = id!!,
-        title = title,
-        description = description,
-        startAt = startAt,
-        endAt = endAt,
-        owner = ContestOwnerResponse.of(owner),
-        visibility = visibility,
-        state = if (startAt.isAfter(LocalDateTime.now())) {
-            ContestState.UPCOMING
-        } else if (endAt.isBefore(LocalDateTime.now())) {
-            ContestState.ENDED
-        } else {
-            ContestState.ONGOING
-        },
-        winner = winner?.let { ContestWinnerResponse.of(it) },
-        operators = contestOperatorRepository.findAllByContest(this).map { ContestOperatorResponse.of(it) },
-        participants = participants.map { ContestParticipantResponse.of(it) },
-        problems = problems.map { ContestProblemResponse.of(it) },
-        createdAt = createdAt,
-        updatedAt = updatedAt
-    )
 }
