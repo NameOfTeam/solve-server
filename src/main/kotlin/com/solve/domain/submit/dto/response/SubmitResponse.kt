c82a6f1a2a5e4af178a7f08a4a2e18b2a1df874e
su080915 <tbr335@naver.com>
2025-01-18 16:45:28 +0900

    fix: submit response

--- src/main/kotlin/com/solve/domain/submit/dto/response/SubmitResponse.kt
+++ src/main/kotlin/com/solve/domain/submit/dto/response/SubmitResponse.kt
@@ -2,15 +2,22 @@ package com.solve.domain.submit.dto.response
 
 import com.solve.domain.submit.domain.entity.Submit
 import com.solve.domain.submit.domain.enums.SubmitState
+import com.solve.global.common.enums.ProgrammingLanguage
 
 data class SubmitResponse(
     val id: Long,
-    val result: SubmitState
+    val result: SubmitState,
+    val language: ProgrammingLanguage,
+    val timeUsage: Long?,
+    val memoryUsage: Long?,
 ) {
     companion object {
         fun of(submit: Submit) = SubmitResponse(
             id = submit.id!!,
-            result = submit.state
+            result = submit.state,
+            language = submit.language,
+            timeUsage = submit.timeUsage,
+            memoryUsage = submit.memoryUsage
         )
     }
 }
